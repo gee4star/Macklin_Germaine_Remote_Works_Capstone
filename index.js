79c33dbb7a2bbeef983a8a8f4343950e84d62483
Your Usernameclear <151780860+gee4star@users.noreply.github.com>
2024-10-15 20:14:41 -0700

    check map fpr possible error

---
+++ index.js
@@ -0,0 +1,8 @@
+let button = document.getElementById('lm');
+let banner=document.getElementById('welcome');
+function welcome(){
+banner.classList.toggle("hidden")
+setTimeout(()=>{
+    banner.classList.toggle("hidden")
+},6000)
+}
